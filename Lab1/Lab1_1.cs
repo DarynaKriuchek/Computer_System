f69405fb16e2a70215c4bc63174757e12918d892
DarynaKriuchek <38790435+DarynaKriuchek@users.noreply.github.com>
2019-03-17 14:02:04 +0200

    Lab1
    Base64, Entropy, FileInfo

---
+++ Lab1/Lab1_1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text="";
+            double len=0.0;
+            double entropy = 0.0;
+            Console.OutputEncoding = Encoding.UTF8;
+            string path = "";
+            Console.WriteLine("Enter number of needed file:\n 1-esenin.txt;\n 2 - bayka.txt;\n 3-cryptography.txt");
+            int file_number = 0;
+            file_number = Int32.Parse(Console.ReadLine());
+            switch (file_number)
+            {
+                case 1:
+                     path = @"C:\Computer system\Lab1_1\Lyrics\esenin.txt";
+                    break;
+                case 2:
+                     path = @"C:\Computer system\Lab1_1\Lyrics\bayka.txt";
+                    break;
+                case 3:
+                     path = @"C:\Computer system\Lab1_1\Lyrics\cryptography.txt";
+                    break;
+                default:
+                    path = @"C:\Computer system\Lab1_1\Lyrics\esenin.txt";
+                    break;
+
+            }
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.UTF8))
+            {
+             
+               text = sr.ReadToEnd();
+
+            }
+            len = text.Length;
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            foreach (char symbol in text)
+            {
+                if (result.ContainsKey(symbol))
+                    result[symbol]++;
+                else
+                    result[symbol] = 1;
+            }
+            foreach(  KeyValuePair<char , int> keyValuePair in result)
+            {
+                entropy += (keyValuePair.Value / len) * Math.Log(len/keyValuePair.Value, 2);
+
+                Console.WriteLine("Symbol = {0}, Frequency = {1},",
+                            keyValuePair.Key, keyValuePair.Value);
+            }
+            double fileinfo = entropy * len / 8;
+            Console.WriteLine("Entropy in bit :{0},\nquantity of info in byte :{1}", entropy, fileinfo);
+            FileInfo file = new FileInfo(path);
+            Console.WriteLine("File name: " + file.Name + "\n File size byte: " + file.Length);
+        }
+    }
+}
---
+++ Lab1/Lab1_2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string base64alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            string path = "";
+            string pathOut = "";
+            Console.WriteLine("Enter number of needed file:\n 1-esenin.txt;\n 2 - bayka.txt;\n 3-cryptography.txt");
+            int file_number = 0;
+            file_number = Int32.Parse(Console.ReadLine());
+            switch (file_number)
+            {
+                case 1:
+                    path = @"C:\Computer system\Lab1_2\Lyrics\esenin.txt.bz2";
+                    pathOut = @"C:\Computer system\Lab1_2\Lyrics\eseninBase64.txt";
+                    break;
+                case 2:
+                    path = @"C:\Computer system\Lab1_2\Lyrics\bayka.txt.bz2";
+                    pathOut = @"C:\Computer system\Lab1_2\Lyrics\baykaBase64.txt";
+                    break;
+                case 3:
+                    path = @"C:\Computer system\Lab1_2\Lyrics\cryptography.txt.bz2";
+                    pathOut = @"C:\Computer system\Lab1_2\Lyrics\cryptographyBase64.txt";
+                    break;
+                default:
+                    path = @"C:\Computer system\Lab1_2\Lyrics\esenin.txt.bz2";
+                    pathOut = @"C:\Computer system\Lab1_2\Lyrics\eseninBase64.txt";
+                    break;
+
+            }
+            List<byte> bytes = new List<byte>();
+            int c;
+            string end = "";
+            using (BinaryReader br = new BinaryReader(new FileStream(path, FileMode.Open)))
+            {
+                for (long i = 0; i < br.BaseStream.Length; i++)
+                    bytes.Add(br.ReadByte());
+            }
+            c = bytes.Count % 3;
+            if (c > 0)
+                for (; c < 3; c++)
+                {
+                    bytes.Add(0);
+                    end += "=";
+                }
+
+            string foo = "";
+
+            for (int i = 0; i < bytes.Count; i = i + 3)
+            {
+                foo += base64alphabet[bytes[i] >> 2];
+                int secondbyte = ((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4);
+                foo += base64alphabet[secondbyte];
+                int thirdbyte = ((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6);
+                foo += base64alphabet[thirdbyte];
+                foo += base64alphabet[bytes[i + 2] & 63];
+            }
+            foo = foo.Substring(0, foo.Length - end.Length) + end;
+            using (StreamWriter swrtr = new StreamWriter(new FileStream(pathOut, FileMode.Create)))
+            {
+                swrtr.Write(foo);
+            }
+        }
+    }
+
+ }
+
